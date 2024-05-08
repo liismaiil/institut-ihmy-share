e4cdc5dd0e7dbe6afc650a4e9902f40edfd18c32
liismaiil <li.1ismai3il@gmail.com>
2024-05-08 20:25:20 +0200

    2024-05-08 update mydars fikh maliki / second / matn Ibn 3achir updated

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,8 +1,8 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import clsx from 'clsx'
+import clsx from 'clsx';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export function Navigation({ navigation, className }) {
   let router = useRouter()
@@ -17,18 +17,26 @@ export function Navigation({ navigation, className }) {
         {navigation.map((section, index) => (
           <li key={section.title} style={{ cursor: 'pointer' }} onClick={() => setSubMenu((prev) => {
             console.log({ prev, index, subMenu });
-             if (typeof subMenu === 'undefined' || typeof prev === 'undefined') {
+            if (typeof subMenu === 'undefined' || typeof prev === 'undefined') {
               return index
             } else {
               console.log({ prev, index, subMenu });
               return index
-
-
             }
           })
+
           }>
             <h2 className="font-display font-medium text-slate-900 dark:text-white">
-              {section.title}
+              <Link
+                href={section.links[0].href}
+                className={clsx(
+                  'block w-full pl-3.5 before:pointer-events-none before:absolute before:-left-1 before:top-1/2 before:h-1.5 before:w-1.5 before:-translate-y-1/2 before:rounded-full',
+                  section.links[0].href === router.pathname
+                    ? 'font-semibold text-sky-500 before:bg-sky-500'
+                    : 'text-slate-500 before:hidden before:bg-slate-300 hover:text-slate-600 hover:before:block dark:text-slate-400 dark:before:bg-slate-700 dark:hover:text-slate-300'
+                )}
+              >{section.title}
+              </Link>
             </h2>
             {subMenu !== -1 && subMenu === index &&
               <ul
